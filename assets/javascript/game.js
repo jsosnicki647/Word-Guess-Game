4fd06777ec753d799a1e256537034224628c1244
Jesse Sosnicki <jesse.sosnicki@gmail.com>
2019-07-06 15:41:02 -0400

    changed multiple arrays into on array of objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,20 +7,66 @@ var isGuessCorrect= []
 var incorrectKeyStrokes = []
 var correctKeyStrokes = []
 var isContinue
-var lastChance = ["He created the Krabby Patty.","She is a karate master.","He is Spongebob's best friend.","He is Bikini Bottom's lifeguard","He loves to play clarinet","He is Squidward's high-class cousin", "He lives in a pineapple under the sea.","He is the captian of a ghostly pirate ship.","He is the king of the sea."]
+var currentSolution
+
 
 var solutions = [
     {
         name: "EUGENE KRABS",
-        hint: "He is the creator of the Krabby Patty."
+        hint: "He is the creator of the Krabby Patty.",
+        pic: "assets/images/mrkrabs.png"
+    },
+    {
+        name: "SANDY CHEEKS",
+        hint: "She is a karate master.",
+        pic: "assets/images/sandycheeks.png"
+    },
+    {
+        name: "PATRICK STAR",
+        hint: "He is Spongbob's best friend.",
+        pic: "assets/images/patrick.png"
+    },
+    {
+        name: "LARRY THE LOBSTER",
+        hint: "He is Bikini Bottom's lifeguard.",
+        pic: "assets/images/larry.jpg"
+    },
+    {
+        name: "SQUIDWARD TENTACLES",
+        hint: "He loves to play the clarinet.",
+        pic: "assets/images/squidward.png"
+    },
+    {
+        name: "SQUILLIAM FANCYSON",
+        hint: "He is Squidward's high-class cousin",
+        pic: "assets/images/squilliam.jpg"
+    },
+    {
+        name: "SPONGEBOB SQUAREPANTS",
+        hint: "He lives in a pineapple under the sea.",
+        pic: "assets/images/spongebob.png"
+    },
+    {
+        name: "THE FLYING DUTCHMAN",
+        hint: "He is the captain of a ghostly pirate ship.",
+        pic: "assets/images/flyingdutchman.jpg"
+    },
+    {
+        name: "KING NEPTUNE",
+        hint: "He is the king of the sea.",
+        pic: "assets/images/kingneptune.jpg"
     }
 ]
 
 // selects word for the game //
 function pickWord(){
+    document.getElementById("answer").innerHTML =  ""
     currentSolution = solutions[Math.floor(Math.random()*solutions.length)].name
     numUnsolved = currentSolution.length - 1
     console.log(numUnsolved)
+    console.log(currentSolution.length)
+    console.log(currentSolution)
+
     
     for (i = 0; i < currentSolution.length; i++){
         
@@ -38,27 +84,44 @@ function pickWord(){
 pickWord()
 
 function replay(){
-    isContinue = confirm("Play again?")
-    // checks if user has chosen to continue //
-    if(isContinue){
-        guessesRemaining = 5
-        document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining
-        document.getElementById("hint").innerHTML = ""
-        document.getElementById("ans-img").src = ""
-        document.getElementById("key-strokes").innerHTML = ""
-        incorrectKeyStrokes = []
-        correctKeyStrokes = []
-        pickWord()
-    }
-    else{
-        alert("bye );")
-        window.close()
-    }
+    answerPlaceholder = []
+    document.getElementById("ans-img").src = solutions[findWithAttr(solutions, "name", currentSolution)].pic
+    document.getElementById("answer").innerText = "Answer: " + currentSolution
+    document.getElementById("hint").innerText = ""
+    setTimeout(function(){
+        isContinue = confirm("Play again?")
+        // checks if user has chosen to continue //
+        if(isContinue){
+            guessesRemaining = 5
+            document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining
+            document.getElementById("answer").innerText = ""
+            document.getElementById("ans-img").src = ""
+            document.getElementById("key-strokes").innerHTML = ""
+            incorrectKeyStrokes = []
+            correctKeyStrokes = []
+            
+            $(document).ready(function(){pickWord()})
+        }
+        else{
+            alert("bye );")
+            window.close()
+        }
+    }, 1000)
 }
 
-$(document).on("keyup", function(e){
+function findWithAttr(array, attr, value) {
+    for(var i = 0; i < array.length; i += 1) {
+        if(array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+$(document).keypress(function(e){
     var selectedKey = String.fromCharCode(e.which).toUpperCase()
-    if(incorrectKeyStrokes.indexOf(selectedKey) == -1 && correctKeyStrokes.indexOf(selectedKey) == -1){    
+    console.log(selectedKey)
+    if(incorrectKeyStrokes.indexOf(selectedKey) == -1 && correctKeyStrokes.indexOf(selectedKey) == -1 && selectedKey != " "){    
         for(var i = 0; i < currentSolution.length; i++){
         // checks if the selected key is part of the solution //
             
@@ -83,12 +146,13 @@ $(document).on("keyup", function(e){
             document.getElementById("guesses-remaining").innerHTML = "Guesses Remaining: " + guessesRemaining
             // checks if no guesses are remaining to ask user to play again //
             if(guessesRemaining == 0){
-                document.getElementById("ans-img").src = "assets/images/spongebob.png"
-                $(document).ready(function(){replay()})
+                
+                alert("You lost. :(")
+                replay()
             }
             // checks if there is one guess remaining and provides hint //
             if(guessesRemaining == 1){
-                document.getElementById("hint").innerHTML = "hint: " + lastChance[solutions.indexOf(currentSolution)]
+                document.getElementById("hint").innerHTML = "hint: " + solutions[findWithAttr(solutions, "name", currentSolution)].hint
             }
         }
         else{
